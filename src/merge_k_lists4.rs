697f739fda7ac68a13ea67419f1de8e94f6ca523
mayrepo <32792759+mayrepo@users.noreply.github.com>
2019-11-21 13:02:20 +0100

    Adding comments

--- src/merge_k_lists4.rs
+++ src/merge_k_lists4.rs
@@ -21,14 +21,15 @@ use std::collections::BinaryHeap;
 impl Solution {
     pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
         
-        let mut a=None;
+        let mut root_node=None;
         
+        //Populate the tree (owned by root_node) by adding every new value yielded by the iterator at the end of the tree (mutably borrowed by last_node)  
         Iterator::new(lists)
-            .fold(&mut a, |a, i| {
-                *a=Some(Box::new(ListNode {next: None, val: i}));
-                &mut a.as_mut().unwrap().next
+            .fold(&mut root_node, |last_node, i| {
+                *last_node=Some(Box::new(ListNode {next: None, val: i}));
+                &mut last_node.as_mut().unwrap().next
             });
-        a
+        root_node
     }
 }
 
@@ -37,6 +38,7 @@ struct Iterator {
 }
 
 impl Iterator {
+    //Populate the BinaryHeap containing the lists, ordering them by the Order defined below
     fn new(lists: Vec<Option<Box<ListNode>>>) -> Iterator {
         Iterator {lists: lists.into_iter().filter_map(|a| a).map(|l| *l).collect()}
     }
@@ -47,16 +49,19 @@ impl std::iter::Iterator for Iterator {
     
     fn next(&mut self) -> Option<i32> {
         
+        //Pop the List with the lowest first element
         let ListNode {val, next} = self.lists.pop()?;
         
         match next {
             None => {},
+            //Add the remaining of the list the BinaryHeap
             Some(next) => self.lists.push(ListNode {val: next.val, next: next.next}),
         }
         Some(val)
     }
 }
 
+//Invered order to get a minimum BinaryHeap because the standard BinaryHeap is a maximum BinaryHeap
 impl Ord for ListNode {
     fn cmp(&self, other: &Self) -> Ordering {
         other.val.cmp(&self.val)
