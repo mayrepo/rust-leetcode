3bfe225577b190e721c783e3491f1a7a539e3f8d
mayrepo <32792759+mayrepo@users.noreply.github.com>
2019-11-21 13:22:18 +0100

    Adding comments

--- src/longuest_consecutivesequence.rs
+++ src/longuest_consecutivesequence.rs
@@ -5,14 +5,27 @@ use std::collections::HashMap;
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
         
+        //HashMaps mapping the minimum and maximum of sequences to the sequences (referenced by counting pointer reference (Rc))
         let mut mins = HashMap::new();
         let mut maxs = HashMap::new();
         
         for num in nums {
-            
-            
-            
             if !maxs.contains_key(&num) && !mins.contains_key(&num) {
+                
+                //Before:
+                //[min1   max1] num [min2   max2]
+                //
+                //After:
+                //[min                       max]
+                
+                //If one or two of the sides do not exist, we replace max or min by num
+                
+                //The case where num falls into an existing sequence, it will create a new sequence that will remain strictly inside the other one because of the previous condition.
+                //There could even be a tree of sequences where parents nodes contain children nodes.
+                
+                //By contruction two sequences can not overlap except for the case of inclusion.
+                //The final result would remain unaffected.
+                
                 let min = maxs.remove(&(num-1)).map(|s: Rc<Sequence>| s.min).unwrap_or(num);
                 let max = mins.remove(&(num+1)).map(|s: Rc<Sequence>| s.max).unwrap_or(num);
 
